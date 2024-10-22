4c3f170cb4fefea86d98b62c7fea6392c2730e42
Mairacol <120331883+Mairacol@users.noreply.github.com>
2024-10-22 14:40:11 -0300

    in

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-
+// Establecer la ruta base para GitHub Pages
 export default defineConfig({
+  base: '/dogs/',  // Ruta base para desplegar en GitHub Pages
   plugins: [react()],
   server: {
     proxy: {
       '/dogs': 'http://localhost:3001', // Ajusta la URL según tu configuración
-         
     },
   },
 });
