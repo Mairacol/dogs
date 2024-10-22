4b2a334d6efd2425908a381ffc3ccd095a6dd5d5
Mairacol <120331883+Mairacol@users.noreply.github.com>
2024-10-22 15:20:51 -0300

    a

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/dogs/', // Cambia por el nombre de tu repositorio
   plugins: [react()],
-  root: './', // Establece la raíz en la carpeta client
   build: {
-    outDir: 'dist', // La carpeta donde se generará la build
+    outDir: 'dist',
   },
 });
